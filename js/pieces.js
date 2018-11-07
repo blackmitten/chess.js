c27935a64cfbeb45b00cf7a9d7fb5314a4e10181
Carl Page <carl.s.page@gmail.com>
2018-11-07 00:50:39 +0000

    more rules

--- js/pieces.js
+++ js/pieces.js
@@ -21,49 +21,46 @@ function Pawn(square, white) {
         return new Pawn(this.square, this.white);
     };
 
+    this.isMoveValid = function (board, square) {
+        if (!isMoveValidGeneric(this, board, square)) {
+            return false;
+        }
+        var dir = white ? -1 : 1;
+        var dx = (square.x - this.square.x);
+        var dy = (square.y - this.square.y);
+        if (dy * dir > 2) {
+            // can never go further than two spaces
+            return false;
+        }
+        if (dy * dir <= 0) {
+            // mustn't go backwards
+            return false;
+        }
+        if (dy * dir > 1 && this.square.y != (white ? 6 : 1)) {
+            // can go two spaces if we haven't moved
+            return false;
+        }
+        var capturedPiece = board.getPieceOnSquare(square);
+        if (dx == 0 && capturedPiece != null) {
+            // can't take forwards
+            return false;
+        }
+        if (dx != 0 && (capturedPiece == null) || Math.abs(dx) > 1) {
+            // can take diagonally
+            return false;
+        }
+
+        return isNothingInTheWay(board, this, square);
+    };
+
     this.getLegalMoves = function (board) {
         var moves = [];
-        var pieceToTake;
-        if (this.white) {
-            if (this.square.y > 0) {
-                if (board.getPieceOnSquare(this.square.offset(0,-1)) == undefined) {
-                    moves.push(this.square.offset(0,-1));
-                    if (this.square.y == 6) {
-                        if (board.getPieceOnSquare(this.square.offset(0,-2)) == undefined) {
-                            moves.push(this.square.offset(0,-2));
-                        }
-                    }
-                }
-                pieceToTake = board.getPieceOnSquare(this.square.offset(-1,-1));
-                if (pieceToTake != undefined && pieceToTake.white != this.white) {
-                    moves.push(this.square.offset(-1,-1));
-                }
-                pieceToTake = board.getPieceOnSquare(this.square.offset(1,-1));
-                if (pieceToTake != undefined && pieceToTake.white != this.white) {
-                    moves.push(this.square.offset(1,-1));
-                }
-            }
-        }
-        else {
-            if (this.square.y < 7) {
-                if (board.getPieceOnSquare(this.square.offset(0,1)) == undefined) {
-                    moves.push(this.square.offset(0,1));
-                    if (this.square.y == 1) {
-                        if (board.getPieceOnSquare(this.square.offset(0,2)) == undefined) {
-                            moves.push(this.square.offset(0,2));
-                        }
-                    }
-                }
-                pieceToTake = board.getPieceOnSquare(this.square.offset(-1,1));
-                if (pieceToTake != undefined && pieceToTake.white != this.white) {
-                    moves.push(this.square.offset(-1,1));
-                }
-                pieceToTake = board.getPieceOnSquare(this.square.offset(1,1));
-                if (pieceToTake != undefined && pieceToTake.white != this.white) {
-                    moves.push(this.square.offset(1,1));
-                }
-            }
-        }
+        var dir = this.white ? -1 : 1;
+        addMove(this, board, moves, this.square.offset(0, dir * 1));
+        addMove(this, board, moves, this.square.offset(0, dir * 2));
+        addMove(this, board, moves, this.square.offset(1, dir * 1));
+        addMove(this, board, moves, this.square.offset(-1, dir * 1));
+
         return moves;
     };
 
@@ -81,34 +78,33 @@ function Rook(square, white) {
 
     this.getLegalMoves = function (board) {
         var moves = [];
-        var x=this.square.x;
-        var y=this.square.y;
-        while(addMoveIfOk(this, board, moves, new Square( ++x, y )));
-        x=this.square.x;
-        y=this.square.y;
-        while(addMoveIfOk(this, board, moves, new Square( --x, y )));
-        x=this.square.x;
-        y=this.square.y;
-        while(addMoveIfOk(this, board, moves, new Square( x, ++y )));
-        x=this.square.x;
-        y=this.square.y;
-        while(addMoveIfOk(this, board, moves, new Square( x, --y )));
+        var x = this.square.x;
+        var y = this.square.y;
+        while (addMove(this, board, moves, new Square(++x, y)));
+        x = this.square.x;
+        y = this.square.y;
+        while (addMove(this, board, moves, new Square(--x, y)));
+        x = this.square.x;
+        y = this.square.y;
+        while (addMove(this, board, moves, new Square(x, ++y)));
+        x = this.square.x;
+        y = this.square.y;
+        while (addMove(this, board, moves, new Square(x, --y)));
 
         return moves;
     };
 }
 
-function addMoveIfOk(piece, board, moves, square) {
-    if ( square.x < 0 || square.x > 7 || square.y < 0 || square.y > 7 ){
-        return false;
-    }
-    var pieceOnSquare = board.getPieceOnSquare(square);
-    if (pieceOnSquare == undefined) {
-        moves.push( square );
+function inBounds(square) {
+    return (square.x >= 0 && square.x <= 7 && square.y >= 0 && square.y <= 7);
+}
+
+function addMove(piece, board, moves, square) {
+    if (square.equals(piece.square)) {
         return true;
     }
-    else if (piece.white != pieceOnSquare.white) {
-        moves.push( square );
+    if (inBounds(square) && piece.isMoveValid(board, square)) {
+        moves.push(square);
         return true;
     }
     return false;
@@ -173,4 +169,40 @@ function King(square, white) {
         return [];
     };
 
+}
+
+function isMoveValidGeneric(piece, board, square) {
+    var valid = inBounds(square);
+    var pieceCaptured = board.getPieceOnSquare(square);
+    if (pieceCaptured != null) {
+        if (pieceCaptured.white == piece.white) {
+            // can't capture our own guys
+            valid = false;
+        }
+    }
+    return valid;
+}
+
+function isNothingInTheWay(board, piece, square) {
+    var dx = square.x - piece.square.x;
+    var dy = square.y - piece.square.y;
+    var xinc = (dx == 0) ? 0 : ((dx < 0) ? -1 : 1);  // ie. either no horizontal move or left / right
+    var yinc = (dy == 0) ? 0 : ((dy < 0) ? -1 : 1);  // ie. either no vertical move or up / down
+
+    if (xinc == 0 && yinc == 0) {
+        throw ("something's way wrong here");
+    }
+    var s = piece.square;
+    var foundSomeone = false;
+    do {    // walk in the specified direction until we get there or we find a piece
+        s = s.offset(xinc, yinc);
+        if (s.equals(square)) {
+            return true;
+        }
+        if (board.getPieceOnSquare(s)) {
+            foundSomeone = true;
+        }
+    } while (!foundSomeone);
+
+    return false;
 }
